010e1568706a534a7120d780d790f18511904613
Artur8090 <88189756+Artur8090@users.noreply.github.com>
2024-05-21 08:06:30 +0200

    Check if works

--- script/script.js
+++ script/script.js
@@ -1,4 +1,7 @@
-function dark_mode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+const btn = document.querySelector('#dark_button');
+    
+function dark_mode(){
+    const body = document.querySelector('body')
+    body.style.backgroundColor = 'HexCode'
+}
+btn.addEventListener('click',dark_mode)
