466fa72c8ce3e1d75c7edbb5eb4f84ffe084ecfa
Sanjeevan <158139276+sanjeevarasa-satkunarasa@users.noreply.github.com>
2024-05-24 16:03:05 +0200

    Issue: Added headings to question formatter

--- question_formatter/app.py
+++ question_formatter/app.py
@@ -6,14 +6,28 @@ def sorted_directory_listing_with_os_listdir(directory):
     sorted_items = sorted(items, reverse=True)
     return sorted_items
 
-sorted_array = sorted_directory_listing_with_os_listdir("eksamensoppgaver/R1") # Set file directory
+sorted_array = sorted_directory_listing_with_os_listdir("eksamensoppgaver/R2") # Set file directory
 
 full_list = []
+heading_list= []
 
-f = open("list.txt", "a")
+i = 0
+while i <= floor(len(sorted_array)/2):
+    år = 2024-i
+    år_str = str(år)
+    heading_list.append("H"+år_str)
+    heading_list.append("V"+år_str)
+    i+=1
+    
+heading_list.pop(0)
 
+file = open("output.txt", "w")
+p = 0
 for i in sorted_array:
-    full_list.append("""<li><a href="/eksamensoppgaver/R1/""" + i + """" target="_blank" download> Oppgave </a></li>\n""") # Remember to correct the file directory here as well
+    full_list.append(heading_list[p])
+    full_list.append("""<li><a href="/eksamensoppgaver/R2/""" + i + """" target="_blank" download> Oppgave </a></li>\n""") # Remember to correct the file directory here as well
+    p+=1    
 
-f.writelines(full_list)
-f.close()
+print(full_list)
+file.writelines(full_list)
+file.close()
