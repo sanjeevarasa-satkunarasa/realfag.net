234406fe44d118000d3b402bee4a7e4ae40121e5
Sanjeevan <158139276+sanjeevarasa-satkunarasa@users.noreply.github.com>
2024-05-27 12:17:53 +0200

    Changed based on sunshine's comments

---
+++ script/dark-mode-script.js
@@ -0,0 +1,8 @@
+function toggleMode() {
+    var themeStyle = document.getElementById("theme-style");
+    if (themeStyle.getAttribute("href") === "styles.css") {
+      themeStyle.href = "styles-dark.css";
+    } else {
+      themeStyle.href = "styles.css";
+    }
+  }
--- script/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('#dark_button');
-    
-function dark_mode(){
-    const body = document.querySelector('body')
-    body.style.backgroundColor = 'HexCode'
-}
-btn.addEventListener('click',dark_mode)
