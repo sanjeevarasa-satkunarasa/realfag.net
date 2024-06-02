67fa878fc1a641a2f378e064272473d3abdba46e
sanjeevarasa-satkunarasa <ssanjeevarasa@proton.me>
2024-06-02 17:20:01 +0200

    Attempted to change report problem to a popup form

---
+++ script/popup.js
@@ -0,0 +1,9 @@
+document.problem_popup.hidden = true;
+
+problem.onclick = function(){
+    document.problem_popup.hidden = false;
+}
+
+close_popup.onclick = function() {
+    document.problem_popup.hidden = true;
+}
