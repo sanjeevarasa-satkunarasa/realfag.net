10586a9dc9ce1ac6b86d4466e02ff13c412bada3
sanjeevarasa-satkunarasa <ssanjeevarasa@proton.me>
2024-09-05 16:25:40 +0200

    Fixed Dark mode for index.html

--- script/script.js
+++ script/script.js
@@ -1,11 +1,13 @@
-function toggleMode() {
-  var themeStyle = document.getElementById("theme-style");
+function toggleModeIndex() {
+  let themeStyle = document.getElementById("theme-style");
   if (themeStyle.getAttribute("href") === "css/styles.css") {
       themeStyle.href = "css/styles-dark.css";
-      document.getElementById("dark_button_img").src = "/media/light_darkmode.png";
+      document.getElementById("dark_mode_icon").src = "/media/dark_mode_dark.png"
+      document.getElementById("dark_mode_discord").src = "/media/discord_logo_dark.png"
   } else if (themeStyle.getAttribute("href") === "css/styles-dark.css"){
       themeStyle.href = "css/styles.css";
-      document.getElementById("dark_button_img").src = "/media/dark_darkmode.png";
+      document.getElementById("dark_mode_icon").src = "/media/dark_mode_light.png"
+      document.getElementById("dark_mode_discord").src = "/media/discord_logo_light.png"
   }
 }
 
@@ -17,8 +19,4 @@ function popUp() {
 function popUpClose() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("hide");
-}
-
-function closeBanner() {
-  document.querySelector(".banner").style.display = "none";
 }
